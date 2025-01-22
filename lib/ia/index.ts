773b910461f8fc720e8ed52a5f8ace4ee4b5f3db
Nicolas <nicolas.herissard@gmail.com>
2025-01-22 23:00:46 +0100

    ajout envoie message en temps reel

--- lib/ia/index.ts
+++ lib/ia/index.ts
@@ -1,6 +1,5 @@
 import OpenAI from "openai";
 
 export const AIConfig = new OpenAI({
-    // apiKey: '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',
-    // dangerouslyAllowBrowser: true,
+    
 })
